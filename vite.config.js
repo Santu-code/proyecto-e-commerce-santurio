1a3613b49c1dbe6a48bbe2bc2a32512b2f109e3c
Enol Santurio <enolsantufer@gmail.com>
2026-01-18 18:38:47 +0100

    intentando areglar error img

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'proyecto-e-commerce-santurio',
+  base: '/proyecto-e-commerce-santurio/',
   plugins: [react()],
 })
